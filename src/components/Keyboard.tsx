dd1540dfbe8ac7361fb324c6a5535757655f6d38
vicktor349 <ayomidevictor349@gmail.com>
2024-09-14 21:50:38 +0100

    mapped through letters of keys

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from "../styles/Keyboard.module.css"
+
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
 
 const Keyboard = () => {
     return (
-        <div>Keyboard</div>
+        <div className='grid grid-cols-auto-fit-minmax gap-2'>
+            {KEYS.map(key => (
+                <button className={`${styles.btn}`} key={key}>{key}</button>
+            ))}
+        </div>
     )
 }
 
