e5ab2f4454d049770ac5dce2ea2ce9d3485ad5de
vicktor349 <ayomidevictor349@gmail.com>
2024-09-14 21:49:01 +0100

    displayed the words for the hangman

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const HangmanWord = () => {
+    const word = "hello"
+    const guessedLetters = ["l"]
     return (
-        <div>HangmanWord</div>
+        <div className='flex gap-1 text-8xl font-bold capitalize font-mono'>
+            {word.split("").map((letter, index) => (
+                <span className='border-b border-black' key={index}>
+                    <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
+                </span>
+            ))}
+        </div>
     )
 }
 
