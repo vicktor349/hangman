79ef567df793067ae88a7f8b8668d92a3f62358d
vicktor349 <ayomidevictor349@gmail.com>
2024-09-17 13:25:02 +0100

    Logic for hangman game

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import word from "./wordList.json"
+import HangmanDrawing from './components/HangmanDrawing';
+import HangmanWord from './components/HangmanWord';
+import Keyboard from './components/Keyboard';
+
+function getWord() {
+  return word[Math.floor(Math.random() * word.length)]
+}
+
+
 
 function App() {
+
+  // GETTING RANDOM WORD LIST IN JSON
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+
+  // GETTING WORD Guessed
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
+
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }, [guessedLetters, isWinner, isLoser])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (!key.match(/^[a-z]$/)) return
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+    document.addEventListener("keypress", handler)
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (key !== "Enter") return
+      setGuessedLetters([])
+      setWordToGuess(getWord())
+    }
+    document.addEventListener("keypress", handler)
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+
   return (
-    <div>
-      hello
+    <div className='w-[800px] flex flex-col gap-8 mx-auto items-center'>
+      <p>Press Enter to get a new word!</p>
+      <div className='text-3xl text-center'>
+        {isWinner && "Winner! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+      <div className="self-stretch">
+        <Keyboard
+          activeLetter={guessedLetters.filter(letter => wordToGuess.includes(letter))}
+          inactiveLetters={incorrectLetters}
+          disabled={isWinner || isLoser}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
     </div>
 
   );
