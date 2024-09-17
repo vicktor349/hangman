cef54292a5270cdcfa9a5be742b69667e1f4cafb
vicktor349 <ayomidevictor349@gmail.com>
2024-09-17 11:24:00 +0100

    guessedletters and word to guess props

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const HangmanWord = () => {
+
+type HangmanDrawingProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+}
+
+const HangmanWord = ({ guessedLetters, wordToGuess }: HangmanDrawingProps) => {
     const word = "hello"
-    const guessedLetters = ["l"]
     return (
         <div className='flex gap-1 text-8xl font-bold capitalize font-mono'>
-            {word.split("").map((letter, index) => (
+            {wordToGuess.split("").map((letter, index) => (
                 <span className='border-b border-black' key={index}>
                     <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
                 </span>
