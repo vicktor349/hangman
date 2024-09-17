8529473db30d5396ea1a3347abc8b7e7b95374af
vicktor349 <ayomidevictor349@gmail.com>
2024-09-17 13:23:54 +0100

    Revealed word after losing the game and also displaying the word in red

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -4,15 +4,18 @@ import React from 'react'
 type HangmanDrawingProps = {
     guessedLetters: string[]
     wordToGuess: string
+    reveal?: boolean
 }
 
-const HangmanWord = ({ guessedLetters, wordToGuess }: HangmanDrawingProps) => {
-    const word = "hello"
+const HangmanWord = ({ guessedLetters, wordToGuess, reveal = false }: HangmanDrawingProps) => {
     return (
         <div className='flex gap-1 text-8xl font-bold capitalize font-mono'>
             {wordToGuess.split("").map((letter, index) => (
                 <span className='border-b border-black' key={index}>
-                    <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
+                    <span
+                        style={{ visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden", color: !guessedLetters.includes(letter) && reveal ? "red" : "black" }}>
+                        {letter}
+                    </span>
                 </span>
             ))}
         </div>
